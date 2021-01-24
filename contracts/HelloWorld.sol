f7e216e1695e181ca6d2afaa1e40567febc3bd86
alexautom <alekoukos1@gmail.com>
2021-01-25 01:20:55 +0200

    2nd update

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.4.22 <0.9.0;
-
+// SPDX-License-Identifier: MIT
 
 
 contract HelloWorld {
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,8 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-const MNEMONIC = '0xE0530AE6Ee9638E64567c104a7bf25544710c67A';
+const MNEMONIC = 'tip group nice dignity candy spike safe multiply give veteran obscure winter';
 
 module.exports = {
+  
   networks: {
     development: {
       host: "127.0.0.1",
@@ -10,10 +11,16 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/65c1b7f8d0844d4987bb7b4510f7e44b")
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/65c1b7f8d0844d4987bb7b4510f7e44b")
       },
       network_id: 3,
       gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: 'JIK9JBMTI3N8GJ5FRJ6XADPZ73TMUFZK5M'
   }
 };
